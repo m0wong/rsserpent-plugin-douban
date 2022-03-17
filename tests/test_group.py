e82aede882546927dc8d00e01efd11f0a286e8a3
m0wong <m0wong@users.noreply.github.com>
2022-03-17 12:48:47 +0800

    Update test_group.py

--- tests/test_group.py
+++ tests/test_group.py
@@ -6,5 +6,4 @@ def test_group(client: TestClient) -> None:
     response = client.get("/douban/group/727241")
     assert response.status_code == 200
     assert response.headers["Content-Type"] == "application/xml"
-    # assert response.text.count("我真见过世面小组 - 豆瓣") == 1
-    assert "<title>\n    我真见过世面小组 - 豆瓣\n</title>" in response.text
+    assert "<title>烂照拯救小组小组的讨论</title>" in response.text
